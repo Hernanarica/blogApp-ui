60a116d580a2ace4ad8004294f130732211e1699
Hernán Arica <hernanaricammm@gmail.com>
2022-10-27 22:41:06 -0300

    feat: se agrega el componente de comentarios

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export * from './Home';
+export * from './Login';
+export * from './TextEditor.jsx';
+export * from './Layout';
+export * from './Profile';
+export * from './Register';
+export * from './Dashboard';
