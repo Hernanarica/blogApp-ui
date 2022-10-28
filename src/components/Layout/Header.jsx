92bd6d506cc03d4ee71c67cb6eaadbe11759e594
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-10-27 23:45:55 -0300

    fix: se realiza un fix sobre el componente Navbar

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -13,7 +13,7 @@ export function Header() {
 	};
 	
 	return (
-		<header className="bg-gray-800 text-white px-4 py-2">
+		<header className="relative px-4 py-2 w-full bg-gray-800 text-white">
 			<nav className="max-w-7xl md:mx-auto">
 				<div className="md:hidden flex items-center justify-between">
 					<button className="align-middle" onClick={ handleMenu }>
@@ -28,7 +28,7 @@ export function Header() {
 					}
 				</div>
 				
-				<ul className={ `${ isOpen ? 'block' : 'hidden' } mt-2 space-y-2 md:flex md:items-center md:gap-3 md:mt-0 md:space-y-0` }>
+				<ul className={ `absolute top-[56px] left-0 w-full h-fit bg-gray-800 text-white px-4 py-3 ${ isOpen ? 'block' : 'hidden' } space-y-2 md:static md:px-0 md:py-0 md:flex md:items-center md:gap-3 md:space-y-0 z-[2]` }>
 					{ !isAuthenticated ? <NavbarPublic isOpen={ isOpen } /> : <NavbarProtected isOpen={ isOpen } /> }
 				</ul>
 				
--- src/components/Utils/Wrapper.jsx
+++ src/components/Utils/Wrapper.jsx
@@ -1,6 +1,6 @@
 export function Wrapper({ children }) {
 	return (
-		<section className="max-w-7xl md:mx-auto">
+		<section className="max-w-7xl md:mx-auto overflow-hidden">
       { children }
 		</section>
 	);
