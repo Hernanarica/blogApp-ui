a1e6dc44e31d2727706117889268beee3e675b86
Hernán Arica <hernanaricammm@gmail.com>
2022-11-12 23:47:47 -0300

    fix: se establece un cookie que expira a los 365 dias

--- src/helpers/cookiesHelpers.js
+++ src/helpers/cookiesHelpers.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 
 export const setCookie = (name, value) => {
-	Cookies.set(name, value);
+	Cookies.set(name, value, { expires: 365 });
 };
 
 export const getCookie = (name) => {
