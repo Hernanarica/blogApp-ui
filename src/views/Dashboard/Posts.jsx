c46cf62826024eb156903576a77956dba3deacea
Hernán Arica <hernanaricammm@gmail.com>
2022-11-13 21:32:07 -0300

    se agregan los filtros en posts

--- src/views/Dashboard/Posts.jsx
+++ src/views/Dashboard/Posts.jsx
@@ -22,19 +22,19 @@ export function Posts() {
 							type="button"
 							className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 						>
-							Years
+							Todos
 						</button>
 						<button
 							type="button"
 							className="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 						>
-							Months
+							Activos
 						</button>
 						<button
 							type="button"
 							className="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
 						>
-							Days
+							Mejor valorados
 						</button>
 					</span>
 				</div>
