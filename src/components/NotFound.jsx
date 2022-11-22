404998a3e532a03cfcfa23561479726fc77635e8
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-11-22 11:02:25 -0300

    se mejora la ui de la seccion NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
+import { routes } from '../router/types/routes.js';
+
 export function NotFound() {
 	return (
-		<div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+		<div className="min-h-[calc(100vh-58px)] bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
 			<div className="mx-auto max-w-max">
 				<main className="sm:flex">
 					<p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
@@ -9,25 +12,25 @@ export function NotFound() {
 					<div className="sm:ml-6">
 						<div className="sm:border-l sm:border-gray-200 sm:pl-6">
 							<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-								Page not found
+								Página no encontrada
 							</h1>
 							<p className="mt-1 text-base text-gray-500">
-								Please check the URL in the address bar and try again.
+								Verifique la URL en la barra de direcciones e intente nuevamente.
 							</p>
 						</div>
 						<div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-							<a
-								href="#"
+							<Link
+								to={ routes.home }
 								className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 							>
-								Go back home
-							</a>
-							<a
-								href="#"
+								Volver al Blog
+							</Link>
+							<Link
+								to={ routes.home }
 								className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 							>
-								Contact support
-							</a>
+								Contactar a soporte
+							</Link>
 						</div>
 					</div>
 				</main>
